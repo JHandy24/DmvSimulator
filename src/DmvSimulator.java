caaf7a275056fb9d645d9af406f36019e53d8cf1
Jacob Conrad <Jacob.Conrad@quinnipiac.edu>
2021-03-03 18:41:52 -0500

    Added nasty message to the exit message.

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -23,7 +23,7 @@ public class DmvSimulator {
 			System.out.println(i);
 		}
 		
-		System.out.println("You don't have the required paperwork");
+		System.out.println("You don't have the required paperwork. Have an awful day!");
 		
 		System.out.println("Exitting the program...");
 	}
